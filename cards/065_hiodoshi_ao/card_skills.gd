939491685ba8de431d7b4238cc8cadd4ed45a35d
gen <gen0e0@gmail.com>
2026-03-07 16:23:58 +0900

    fix delay

--- cards/065_hiodoshi_ao/card_skills.gd
+++ cards/065_hiodoshi_ao/card_skills.gd
@@ -7,9 +7,9 @@ func _skill_0(ctx: SkillContext) -> SkillResult:
 	var opp: int = 1 - ctx.player
 	if ctx.state.stages[opp].size() >= 3:
 		return SkillResult.done()
-	ctx.emit_cue(AnimationCue.find_card(ctx.source_instance_id).move().to_op_stage())
+	ctx.emit_cue(AnimationCue.find_card(ctx.source_instance_id).move().to_op_stage().with_delay(0.2))
 	ZoneOps.play_to_stage_from_zone(ctx.state, opp, ctx.source_instance_id, ctx.recorder)
 	var drawn_iid: int = ZoneOps.draw_card(ctx.state, ctx.player, ctx.recorder)
 	if drawn_iid >= 0:
-		ctx.emit_cue(AnimationCue.make_card(drawn_iid).move().from_deck().to_my_hand().with_delay(0.2))
+		ctx.emit_cue(AnimationCue.make_card(drawn_iid).move().from_deck().to_my_hand())
 	return SkillResult.done()
